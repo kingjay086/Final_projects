660370044e2f7ebddfd979450a19b29364e0a4be
kingjay <140686923+kingjay086@users.noreply.github.com>
2025-07-28 23:40:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,31 @@
 import streamlit as st
-import pickle
 import pandas as pd
+import pickle
+import os
+import requests
 
-# Load the preprocessed data
+def download_file_from_google_drive(url, destination):
+    if not os.path.exists(destination):
+        st.info(f"Downloading {destination}...")
+        r = requests.get(url, allow_redirects=True)
+        open(destination, 'wb').write(r.content)
+        st.success(f"{destination} downloaded.")
+
+#  GDrive links
+MOVIES_URL = "https://drive.google.com/uc?export=download&id=1k5pA6DLaYxtxrbgTh55JYbXmmOoWq08J"
+SIMILARITY_URL = "https://drive.google.com/uc?export=download&id=1k5pA6DLaYxtxrbgTh55JYbXmmOoWq08J"
+
+download_file_from_google_drive(MOVIES_URL, "movies.pkl")
+download_file_from_google_drive(SIMILARITY_URL, "similarity.pkl")
+
 movies = pickle.load(open('movies.pkl', 'rb'))
 similarity = pickle.load(open('similarity.pkl', 'rb'))
 
-# Recommend function
 def recommend(movie):
-    try:
-        movie_index = movies[movies['title'] == movie].index[0]
-        distances = list(enumerate(similarity[movie_index]))
-        movie_list = sorted(distances, key=lambda x: x[1], reverse=True)[1:6]
-        return [movies.iloc[i[0]].title for i in movie_list]
-    except:
-        return []
+    index = movies[movies['title'] == movie].index[0]
+    distances = similarity[index]
+    movie_list = sorted(list(enumerate(distances)), reverse=True, key=lambda x: x[1])[1:6]
+    return [movies.iloc[i[0]].title for i in movie_list]
 
 # Streamlit UI
 st.set_page_config(page_title="Movie Recommender", page_icon="🎬")
